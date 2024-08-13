2e34a91be25222cc032cc504a7b4d2ad3620e095
Tomayoladef <117799966+Tomayoladef@users.noreply.github.com>
2024-08-13 12:38:51 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,24 @@
+#!/usr/bin/python3
+"""
+Uses Reddit API to print the number of subscribers of a subreddit
+"""
+import requests
 
+
+def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'request'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    data = response.json().get("data")
+    if data is None:
+        return 0
+    
+    num_subs = data.get("subscribers")
+    return num_subs
